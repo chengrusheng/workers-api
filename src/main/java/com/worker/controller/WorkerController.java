3a62eb5173ec49d671f2b5e68fcb67d9a9d4bef4
程如胜 <1527089598@qq.com>
2023-12-21 13:53:18 +0800

    导出

--- src/main/java/com/worker/controller/WorkerController.java
+++ src/main/java/com/worker/controller/WorkerController.java
@@ -1,19 +1,22 @@
 package com.worker.controller;
 
 
+import cn.afterturn.easypoi.excel.ExcelExportUtil;
+import cn.afterturn.easypoi.excel.entity.ExportParams;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.worker.entity.Role;
 import com.worker.entity.Worker;
 import com.worker.service.WorkerService;
 import com.worker.utils.Result;
 import com.worker.vo.query.WorkerQueryVo;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import org.springframework.stereotype.Controller;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * <p>
@@ -25,6 +28,7 @@ import org.springframework.stereotype.Controller;
  */
 @RestController
 @RequestMapping("/api/worker")
+//@Api(value = "职工接口", tags = {"职工接口"})
 public class WorkerController {
 
     @Autowired
@@ -36,6 +40,7 @@ public class WorkerController {
      * @return
      */
     @GetMapping("/list")
+//    @ApiOperation("分页查询职工列表")
     public Result list(WorkerQueryVo workerQueryVo) {
         //创建分页对象
         IPage<Worker> page = new Page<Worker>
@@ -53,6 +58,7 @@ public class WorkerController {
      * @return
      */
     @PostMapping("/add")
+//    @ApiOperation("新增职工")
     public Result add(@RequestBody Worker worker){
         if(workerService.save(worker)){
             return Result.ok().message("新增成功");
@@ -66,6 +72,7 @@ public class WorkerController {
      * @return
      */
     @PutMapping("/update")
+//    @ApiOperation("修改职工")
     public Result update(@RequestBody Worker worker){
         if(workerService.updateById(worker)){
             return Result.ok().message("编辑成功");
@@ -79,11 +86,33 @@ public class WorkerController {
      * @return
      */
     @DeleteMapping("/delete/{id}")
+//    @ApiOperation("删除职工")
     public Result delete(@PathVariable Long id){
         if(workerService.removeById(id)){
             return Result.ok().message("删除成功");
         }
         return Result.error().message("删除失败");
+    }
+
+    @GetMapping("/export")
+//    @ApiOperation("导出")
+    public void exportExcel(HttpServletResponse response, Worker worker) throws Exception {
+        List<Worker> workerList = workerService.list(new QueryWrapper<Worker>()
+                .like(StringUtils.isNotEmpty(worker.getEmpName()), "emp_name", worker.getEmpName())
+                .eq(StringUtils.isNotEmpty(worker.getDeptName()), "dept_name", worker.getDeptName())
+                .eq(StringUtils.isNotEmpty(worker.getEmpDegreeName()), "emp_degree_name", worker.getEmpDegreeName()));
+        // 创建Excel文档
+        Workbook workbook = ExcelExportUtil.exportExcel(new ExportParams("职工数据", "职工数据"), Worker.class, workerList);
+
+        // 设置响应头信息
+        response.setContentType("application/vnd.ms-excel");
+        response.setHeader("Content-Disposition", "attachment; filename=职工数据.xlsx");
+
+        // 将Excel文档写入响应流中
+        ServletOutputStream outputStream = response.getOutputStream();
+        workbook.write(outputStream);
+        outputStream.flush();
+        outputStream.close();
     }
 
 }
--- src/main/java/com/worker/entity/Worker.java
+++ src/main/java/com/worker/entity/Worker.java
@@ -1,5 +1,6 @@
 package com.worker.entity;
 
+import cn.afterturn.easypoi.excel.annotation.Excel;
 import com.alibaba.fastjson.annotation.JSONField;
 import com.alibaba.fastjson.serializer.ToStringSerializer;
 import com.baomidou.mybatisplus.annotation.TableName;
@@ -20,6 +21,7 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @TableName("worker")
+//@ApiModel("职工信息")
 public class Worker implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -29,31 +31,42 @@ public class Worker implements Serializable {
      */
     @TableId(value = "id", type = IdType.ASSIGN_ID)
     @JSONField(serializeUsing = ToStringSerializer.class)
+//    @ApiModelProperty("id")
     private Long id;
 
     /**
      * 职员姓名
      */
+    @Excel(name = "姓名", orderNum = "0")
+//    @ApiModelProperty("姓名")
     private String empName;
 
     /**
      * 性别
      */
+    @Excel(name = "性别", orderNum = "1")
+//    @ApiModelProperty("性别")
     private String sex;
 
     /**
      * 年龄
      */
+    @Excel(name = "年龄", orderNum = "2")
+//    @ApiModelProperty("年龄")
     private Integer age;
 
     /**
      * 部门名称
      */
+    @Excel(name = "部门名称", orderNum = "3")
+//    @ApiModelProperty("部门名称")
     private String deptName;
 
     /**
      * 学历
      */
+    @Excel(name = "学历", orderNum = "4")
+//    @ApiModelProperty("学历")
     private String empDegreeName;
 
 
